bbbd23fd4d20f61ffe9d57f1d8530c2b5ad1e92f
免貴姓劉丶 <470549046@qq.com>
2023-02-13 15:56:12 +0800

    first commit

---
+++ src/Test.php
@@ -0,0 +1,9 @@
+<?php
+namespace Liuy\Test;
+
+class Test {
+    public function index()
+    {
+        echo 'tes ok~';
+    }
+}
---
+++ src/helpers.php
@@ -0,0 +1,6 @@
+<?php
+
+function help()
+{
+    echo 'is run help function~';
+}
